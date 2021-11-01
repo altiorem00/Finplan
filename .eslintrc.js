50f106fb29838f0251e1ad24fa833893e90acfcc
Kuzhovnik Nikita <altiorem00@mail.ru>
2021-11-01 13:04:17 +0500

    hello world

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: ["plugin:vue/essential", "eslint:recommended"],
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ]
+};
---
+++ src/utils/message.plugin.js
@@ -0,0 +1,12 @@
+import localizeFilter from "@/filters/localize.filter";
+
+export default {
+  install(Vue) {
+    Vue.prototype.$message = function (html) {
+      M.toast({ html });
+    };
+    Vue.prototype.$error = function (html) {
+      M.toast({ html: `[${localizeFilter("Error")}]: ${html}` });
+    };
+  },
+};
